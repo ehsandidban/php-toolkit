b4e786c118b608304ae2448c39637c5c200e16d1
unknown <e93.didban@yahoo.com>
2026-02-12 18:29:55 +0330

    first commit of send sms function with ghasedak sms panel

---
+++ sms-service/ghasedak-send-sms.php
@@ -0,0 +1,44 @@
+<?php
+
+# example for a template with 1 param
+# if your template has more params, esaily you can add.
+function send_sms($phone, $first_name)
+{
+    $api_key = 'type_your_api_key_here';
+    $template_name = 'type_your_template_name_here';
+
+    $url = 'https://gateway.ghasedak.me/rest/api/v1/WebService/SendOtpWithParams';
+
+    $headers = [
+        'accept: text/plain',
+        'ApiKey: ' . $api_key,
+        'Content-Type: application/json'
+    ];
+
+    $post_fields = [
+        "receptors" => [
+            [
+                "mobile" => $phone,
+                "clientReferenceId" => uniqid()
+            ]
+        ],
+        "templateName" => $template_name,
+        "param1" => $first_name,
+        "isVoice" => false,
+        "udh" => false
+    ];
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_fields));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    return [
+        'status_code' => $http_code,
+        'response' => $response
+    ];
+}
