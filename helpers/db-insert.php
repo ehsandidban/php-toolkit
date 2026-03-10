809e33b0b6be173eaf2437ecf31f63466935fcab
unknown <e93.didban@yahoo.com>
2026-03-10 19:21:03 +0330

    add file (db-insert.php) to use for insert data into database tables

---
+++ helpers/db-insert.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Function to use for insert data in database
+ */
+
+function db_insert(string $table, array $data)
+{
+    // generate columns
+    $sql_columns = implode(', ', array_keys($data));
+
+    //generate values
+    $vals = array_values($data);
+
+    $sql_vals = [];
+    foreach ($vals as $val) {
+        if ($val === NULL) {
+            $val = 'NULL';
+        }
+        $sql_vals[] = "'$val'";
+    }
+
+    $sql_values = implode(', ', $sql_vals);
+
+    // create query
+
+    $sql = "INSERT INTO $table ($sql_columns) VALUES ($sql_values)";
+    print_r($sql);
+}
+
+// Test
+db_insert('users',[
+    'name' => 'Ehsan',
+    'family' => 'Didbabn',
+    'age' => 32,
+    'job' => 'Programmer'
+]);
