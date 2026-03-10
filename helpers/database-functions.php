f5bc9ff9f0c455ea7751c7be7ab961cd41bfc71c
unknown <e93.didban@yahoo.com>
2026-03-10 20:48:04 +0330

    add function (db_update) to create mysql update query

--- helpers/database-functions.php
+++ helpers/database-functions.php
@@ -23,16 +23,61 @@ function db_insert(string $table, array $data)
 
     $sql_values = implode(', ', $sql_vals);
 
-    // create query
+    // generate query
 
     $sql = "INSERT INTO $table ($sql_columns) VALUES ($sql_values)";
     print_r($sql);
 }
 
+/**
+ * function for create mysql update query
+ */
+function db_update($table, $data, $where)
+{
+    // generate SET of query
+    $set_sql = '';
+    foreach ($data as $key => $value) {
+        if ($value === NULL) {
+            $set_sql .= "$key = NULL, ";
+            continue;
+        }
+        $set_sql .= "$key = '$value', ";
+    }
+    $set_sql = trim($set_sql, ', ');
+
+    // generate WHERE of query
+    if (is_array($where)) {
+        $where_sql = '';
+        foreach ($where as $key => $value) {
+            if ($value === NULL) {
+                $where_sql .= "$key IS NULL AND ";
+                continue;
+            }
+            $where_sql .= "$key = '$value' AND ";
+        }
+        $where_sql = trim($where_sql, 'AND ');
+    } else {
+        $where_sql = $where;
+    }
+
+    // generate query
+    $sql = "UPDATE $table SET $set_sql WHERE $where_sql";
+    print_r($sql);
+}
+
 // Test
-db_insert('users',[
-    'name' => 'Ehsan',
-    'family' => 'Didbabn',
-    'age' => 32,
-    'job' => null
-]);
+// db_insert('users',[
+//     'name' => 'Ehsan',
+//     'family' => 'Didbabn',
+//     'age' => 32,
+//     'job' => null
+// ]);
+
+// db_update('users', [
+//     'name' => 'x',
+//     'family' => 'y',
+//     'phone' => null
+// ], [
+//     'uid' => null,
+//     'phone' => null
+// ]);
